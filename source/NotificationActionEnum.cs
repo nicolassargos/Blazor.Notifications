ec4c23bf9bcbfc843c245d794299344d318eb2f5
nicolassargos <nicolassargos@hotmail.com>
2023-07-23 14:59:24 +0200

    small adjustments

--- source/NotificationActionEnum.cs
+++ source/NotificationActionEnum.cs
@@ -1,5 +1,8 @@
 ﻿namespace Append.Blazor.Notifications
 {
+    /// <summary>
+    /// Predefined action values to avoid some typos
+    /// </summary>
     public class NotificationActionEnum
     {
         public const string View = "View";
--- source/ServiceCollectionExtensions.cs
+++ source/ServiceCollectionExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace Append.Blazor.Notifications
 {
+    /// <summary>
+    /// Extension methods for AspNet service registration.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddNotifications(this IServiceCollection services)
         {
             return services.AddScoped<INotificationService, NotificationService>();
