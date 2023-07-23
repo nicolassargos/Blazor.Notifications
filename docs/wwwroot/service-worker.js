e42d575e21a9f632992d29faffbe00257d7f0004
nicolassargos <nicolassargos@hotmail.com>
2023-07-23 13:29:35 +0200

    added redirection - wip

--- docs/wwwroot/service-worker.js
+++ docs/wwwroot/service-worker.js
@@ -1,1 +1,24 @@
-﻿self.addEventListener('fetch', () => { });
+﻿/*self.addEventListener('fetch', () => { });*/
+//self.addEventListener(
+//    'notificationclose',
+//    function (event) {
+//        const dismissedNotification = event.notification;
+
+//        console.log("On notification close: ", event.notification.tag);
+
+//        const promiseChain = clients.openWindow('https://www.wowhead.com/diablo-4/guide/classes/barbarian/legendary-aspects');
+//        event.waitUntil(promiseChain);
+//    }
+//);
+
+
+self.addEventListener('notificationclick', (event) => {
+    const clickedNotification = event.notification;
+
+    // Do something as the result of the notification click
+    const promiseChain = clients.openWindow('https://www.wowhead.com/diablo-4/guide/classes/barbarian/legendary-aspects');
+    event.waitUntil(promiseChain);
+
+    clickedNotification.close();
+
+});
